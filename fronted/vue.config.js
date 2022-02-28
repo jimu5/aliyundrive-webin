4cc0c4a0023eeea13c81b873ab75f10e9e3e69ed
Kimiato <853447650@qq.com>
2022-03-01 00:47:18 +0800

    feat: 添加本地代理服务器

--- fronted/vue.config.js
+++ fronted/vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000/', //对应自己的接口
+        changeOrigin: true,
+        ws: true
+      }
+    }
+  }
+});
