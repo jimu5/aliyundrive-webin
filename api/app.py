396fd981c278bf6921ec15d4bb5e199010acf744
Kimiato <853447650@qq.com>
2022-03-01 00:47:38 +0800

    feat: 跨域问题

--- api/app.py
+++ api/app.py
@@ -1,7 +1,9 @@
 from sanic import Sanic
 from aligo import Aligo
+from sanic_cors import CORS
 
 
 # 初始化服务
 alidrive = Aligo()
 app = Sanic("aliyundrive-webin-api")
+CORS(app)
--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,10 @@ from . import app
 from .views import search_file, get_download
 
 
-def add_base_route(handler, uri, methods=["GET"], **ctx_kwargs):
+def add_base_route(handler, uri, methods=['GET', 'OPTIONS'], **ctx_kwargs):
     uri = "/api" + uri
+    if 'OPTIONS' not in methods:
+        methods.append('OPTIONS')
     app.add_route(handler=handler, uri=uri, methods=methods, **ctx_kwargs)
 
 
