fb275b53872d7bc723eb2863086178893860b6c1
Kimiato <853447650@qq.com>
2022-03-02 23:46:45 +0800

    feat: 添加vetur配置

---
+++ vetur.config.js
@@ -0,0 +1,11 @@
+// vetur.config.js
+/** @type {import('vls').VeturConfig} */
+module.exports = {
+    // **optional** default: `[{ root: './' }]`
+    // support monorepos
+    projects: [
+        {
+            root: './frontend'
+        }
+    ]
+}
